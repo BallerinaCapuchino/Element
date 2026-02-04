4fea59685a8fc493b74a85db9cc3f68d79c23972
Макс <pon45424@gmail.com>
2026-02-04 14:22:04 +0300

    Update App with full routing and logic

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+﻿import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Element/', 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+});
